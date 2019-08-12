27caa623180700218f655b00b778ce4482c2616b
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-08-11 23:08:08 -0400

    Adding Excersice 5.2

---
+++ week-5/armstrong-md-top-menu/src/app/app.module.ts
@@ -0,0 +1,41 @@
+/*
+============================================
+; Title:  Exercise 5.2 - Navigation
+; Author: Tyler Armstrong
+; Date:   11 Aug 2019
+; Modified By: N/A
+; Description: Angular Navigation
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
